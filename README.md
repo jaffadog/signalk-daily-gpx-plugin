9230d0d377d9aee83e988e3d8efbe1b078ba7fbb
Jeremy Waters <jaffadog@gmail.com>
2025-09-26 07:49:35 -1000

    add option to save file per voyage add minimum distance option

--- README.md
+++ README.md
@@ -2,43 +2,44 @@
 
 # SignalK Daily GPX Plugin
 
-A [SignalK](https://signalk.org/) plugin that generates a daily [GPX](http://www.topografix.com/gpx.asp) file.
+A [SignalK](https://signalk.org/) plugin that records your track and generates either daily or per-voyage [GPX](http://www.topografix.com/gpx.asp) files.
 
 ## What Does It Do?
 
-The SignalK Daily GPX Plugin records your position on a specified time interval to a temporary buffer on the SignalK server. At midnight, it collects the recorded data and creates a GPX file containing your track for the day. The GPX file is saved on the SignalK server in a folder of your choosing. Over time, you automatically accumulate daily GPX track files documenting your voyages.
-
-Note that GPX files are not created until midnight (per the clock on your SignalK server). So if you look for GPX files before then, you won't find any.
+The SignalK Daily GPX Plugin records your position on a specified time interval to a temporary buffer on the SignalK server. If configured to generate daily GPX files, at midnight, it collects the recorded data and creates a GPX file containing your track for the day. If configured to generate GPX files per-voyage, once the vessel stops, it collects the recorded data and creates a GPX file containing your track for that voyage. The GPX file is saved on the SignalK server in a folder of your choosing. Over time, you automatically accumulate GPX track files documenting your voyages.
 
 ## Configuration
 
 ### GPS Position Source
-
 If there are multiple sources of **navigation.position**, then specify which one we should use. If left blank, all will be used - which may lead to duplicate points in the track, or less time than the specified interval between points. If you have multiple sources of **navigation.position** being reported in SignalK, you should configure **Source Priorities** in SignalK to filter out the duplicate/extraneous source(s).
 
-### Time Interval
-
-Number of minutes between recorded track positions (default is 1 minute).
+### Time Interval (minutes)
+Number of minutes between recorded track positions (default is 1 minutes).
 
 ### Minimum Speed (knots)
+The minimum speed over ground (SOG) required to trigger track recording. This prevents track recording while anchored or docked. If set to 0, the track is recorded regardless of boat speed (default is 0.5 knots).
 
-The minimum speed over ground (SOG) required to trigger track recording. This prevents track recording while anchored or docked. If blank, the track 
-is recorded at the specified time interval regardless of speed. (default is 0.5 knots).
+### Minimum Distance (meters)
+The minimum distance between recorded track points. This prevents track recording while anchored or docked. If set to zero, the track is recorded regardless of distance between points (default is 50 meters).
 
 ### Track Simplification Tolerance (meters)
-
-Simplify the saved track - by removing points where the track is pretty straight and keeping points where direction changes. This produces a much 
-smaller GPX file while maintaining good path resolution. If blank, track simplification will be disabled and the track will be saved at the full 
-recorded resolution (default is 10 meters).
+Simplify the saved track - by removing points where the track is pretty straight and keeping points where direction changes. This produces a much smaller GPX file while maintaining good path resolution. If set to 0, track simplification will be disabled and the track will be saved at the full recorded resolution (default is 10 meters).
 
 ### Folder Path
-
-Folder path to save GPX files in. If left blank, default is:
+Folder path to save gpx files in. If left blank, default is:
 
     $SIGNALK_NODE_CONFIG_DIR/plugin-config-data/signalk-daily-gpx-plugin.
 
+### Record Voyage?
+Create GPX files for each complete voyage rather than every 24 hours (default is false).
+
+### Record Depth?
+Record the current depth (from water surface) at each track point using the Garmin extension format (default is false).
+
 ## Extras
 
+### Webapp
 This plugin includes a SignalK webapp (also named **SignalK Daily GPX Plugin**) which facilitates downloading GPX files and managing the local buffer.
 
+### Recommended Plugins
 If your travels take you across timezones, you could use the [signalk-set-gps-timezone](https://github.com/hoeken/signalk-set-gps-timezone) plugin to automatically update your SignalK server's timezone. That way, GPX files will always be written at midnight in your local timezone - and capture the full day of travel with an ongoing local timezone context.
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const MAX_DEPTH_AGE_IN_MILLIS = 10000;
 
 const DEFAULT_TRACK_INTERVAL = 1;
 const DEFAULT_MINIMUM_SPEED = 0.5;
+const DEFAULT_MINIMUM_DISTANCE = 50;
+const DEFAULT_RECORD_VOYAGE = false;
 const DEFAULT_SIMPLIFICATION_TOLERANCE = 10;
 const DEFAULT_RECORD_DEPTH = false;
 
@@ -23,7 +25,7 @@ const simplify = require('./simplify.js');
 module.exports = function (app) {
     var plugin = {};
     var db;
-    var previousPosition;
+    var lastRecordedPosition;
     var filename;
     var bufferCount = 0;
     var unsubscribes = [];
@@ -31,6 +33,9 @@ module.exports = function (app) {
     var gpsSource;
     var trackInterval;
     var minimumSpeed;
+    var minimumDistance;
+    var recordVoyage;
+    var activeVoyage = false;
     var simplificationTolerance;
     var recordDepth;
     var gpxFolder;
@@ -44,7 +49,9 @@ module.exports = function (app) {
 
         gpsSource = options.gpsSource;
         trackInterval = options.trackInterval || DEFAULT_TRACK_INTERVAL;
-        minimumSpeed = options.minimumSpeed ?? DEFAULT_MINIMUM_SPEED;
+        minimumSpeed = options.minimumSpeed ?? DEFAULT_MINIMUM_SPEED; // ?? acccepts 0 as a valid value and wont overide it
+        minimumDistance = options.minimumDistance ?? DEFAULT_MINIMUM_DISTANCE;
+        recordVoyage = options.recordVoyage || DEFAULT_RECORD_VOYAGE;
         simplificationTolerance = options.simplificationTolerance ?? DEFAULT_SIMPLIFICATION_TOLERANCE;
         recordDepth = options.recordDepth ?? DEFAULT_RECORD_DEPTH;
         gpxFolder = options.gpxFolder ? options.gpxFolder : app.getDataDirPath();
@@ -53,18 +60,17 @@ module.exports = function (app) {
         db = new Database(dbFile, { verbose: app.debug });
         db.prepare('CREATE TABLE IF NOT EXISTS buffer(ts REAL, latitude REAL, longitude REAL, depth REAL)').run();
 
-        var dbVersion = db.pragma('user_version', { simple: true });
-        app.debug('DB Version', dbVersion);
+        // update older versions of the buffer table
+        var hasDepth = db.prepare('select count(*) as count from pragma_table_info(?) where name=?').get('buffer', 'depth').count;
+        app.debug('hasDepth=', hasDepth);
 
-        if (dbVersion < 1) {
+        if (!hasDepth) {
             try {
-                app.debug('updating db to version 1');
+                app.debug('adding depth column');
                 db.prepare("ALTER TABLE buffer ADD COLUMN depth REAL").run();
-                db.pragma('user_version = 1');
-                dbVersion = 1;
             }
             catch (error) {
-                app.error("error updating db", error);
+                app.error(error);
             }
         }
 
@@ -110,20 +116,20 @@ module.exports = function (app) {
     plugin.schema = {
         type: 'object',
         description: `NOTE: The plugin will store track positions in a local buffer and will write the gpx file at 
-            midnight (per the signal K server clock). You can review and download available gpx files in the 
-            SignalK Daily GPX Plugin webapp.`,
-        required: ['trackInterval', 'minimumSpeed', 'simplificationTolerance'],
+            midnight (per the signal K server clock) - or at the completion of the voyage. You can review and download 
+            available gpx files in the SignalK Daily GPX Plugin webapp.`,
+        required: ['trackInterval', 'minimumSpeed', 'minimumDistance', 'simplificationTolerance'],
         properties: {
             gpsSource: {
                 title: 'GPS Position Source',
                 type: 'string',
-                description: 'If there are multiple sources of navigation.position, then specify which one we should use'
+                description: 'If there are multiple sources of navigation.position, then specify which one we should use.'
             },
             trackInterval: {
                 title: 'Time Interval (minutes)',
                 type: 'number',
                 minimum: 0.1,
-                description: `Number of minutes between recorded track positions (default is ${DEFAULT_TRACK_INTERVAL} minutes)`,
+                description: `Number of minutes between recorded track positions (default is ${DEFAULT_TRACK_INTERVAL} minutes).`,
                 default: DEFAULT_TRACK_INTERVAL
             },
             minimumSpeed: {
@@ -131,9 +137,17 @@ module.exports = function (app) {
                 type: 'number',
                 minimum: 0,
                 description: `The minimum speed over ground (SOG) required to trigger track recording. This prevents track 
-                recording while anchored or docked. If set to 0, the track is recorded at the specified time interval regardless 
-                of boat speed. (default is ${DEFAULT_MINIMUM_SPEED} knots)`,
+                recording while anchored or docked. If set to 0, the track is recorded regardless of boat speed (default 
+                is ${DEFAULT_MINIMUM_SPEED} knots).`,
                 default: DEFAULT_MINIMUM_SPEED
+            },
+            minimumDistance: {
+                title: 'Minimum Distance (meters)',
+                type: 'number',
+                minimum: 0,
+                description: `The minimum distance between recorded track points. This prevents track recording while anchored or 
+                docked. If set to zero, the track is recorded regardless of distance between points (default is ${DEFAULT_MINIMUM_DISTANCE} meters).`,
+                default: DEFAULT_MINIMUM_DISTANCE
             },
             simplificationTolerance: {
                 title: 'Track Simplification Tolerance (meters)',
@@ -141,20 +155,26 @@ module.exports = function (app) {
                 minimum: 0,
                 description: `Simplify the saved track - by removing points where the track is pretty straight and keeping points where direction 
                     changes. This produces a much smaller GPX file while maintaining good path resolution. If set to 0, track simplification will be
-                    disabled and the track will be saved at the full recorded resolution (default is ${DEFAULT_SIMPLIFICATION_TOLERANCE} meters)`,
+                    disabled and the track will be saved at the full recorded resolution (default is ${DEFAULT_SIMPLIFICATION_TOLERANCE} meters).`,
                 default: DEFAULT_SIMPLIFICATION_TOLERANCE
-            },
-            recordDepth: {
-                title: 'Record Depth?',
-                type: 'boolean',
-                description: `Record the current depth (from water surface) at each track point using the Garmin extension format (default is ${DEFAULT_RECORD_DEPTH})`,
-                default: DEFAULT_RECORD_DEPTH
             },
             gpxFolder: {
                 title: 'Folder Path',
                 type: 'string',
                 description: `Folder path to save gpx files in. If left blank, default is $SIGNALK_NODE_CONFIG_DIR/plugin-config-data/${plugin.id}.`
-            }
+            },
+            recordVoyage: {
+                type: "boolean",
+                title: `Record Voyage?`,
+                description: `Create GPX files for each complete voyage rather than every 24 hours (default is ${DEFAULT_RECORD_VOYAGE}).`,
+                default: DEFAULT_RECORD_VOYAGE
+            },
+            recordDepth: {
+                title: 'Record Depth?',
+                type: 'boolean',
+                description: `Record the current depth (from water surface) at each track point using the Garmin extension format (default is ${DEFAULT_RECORD_DEPTH}).`,
+                default: DEFAULT_RECORD_DEPTH
+            },
         }
     };
 
@@ -222,6 +242,11 @@ module.exports = function (app) {
         var position = update.values[0].value;
         position.ts = new Date(update.timestamp).getTime();
 
+        if (!validPosition(position)) {
+            app.error("ERROR: Invalid position:", position);
+            return;
+        }
+
         if (recordDepth) {
             var depth = app.getSelfPath("environment.depth.belowSurface");
             if (depth && depth.value && depth.timestamp && Date.now() - new Date(depth.timestamp).getTime() < MAX_DEPTH_AGE_IN_MILLIS) {
@@ -229,26 +254,71 @@ module.exports = function (app) {
             }
         }
 
-        if (previousPosition && new Date(position.ts).getDate() != new Date(previousPosition.ts).getDate() && bufferCount > 1) {
-            app.debug('starting a new day - time to write the gpx file');
+        if (!lastRecordedPosition) {
+            lastRecordedPosition = position;
+        }
+
+        var sog = app.getSelfPath("navigation.speedOverGround").value * KNOTS_PER_M_PER_S;
+        var distance = getDistanceFromLatLonInMeters(lastRecordedPosition.latitude, lastRecordedPosition.longitude, position.latitude, position.longitude);
+        var isNewDay = new Date(position.ts).getDate() != new Date(lastRecordedPosition.ts).getDate();
+        var minutesSinceLastRecordedPosition = (Date.now() - lastRecordedPosition.ts) / 1000 / 60;
+        var vesselStopped = minutesSinceLastRecordedPosition > 3 * trackInterval;
+        // 50 meters / 1 minutes = 1.62 knots
+        // 50 meters / 2 minutes = 0.81 knots
+        // 50 meters / 3 minutes = 0.54 knots
+        // 50 meters / 5 minutes = 0.32 knots
+
+        app.debug(`bufferCount=${bufferCount} sog=${(sog || 0).toFixed(2)} distance=${(distance || 0).toFixed(2)} isNewDay=${isNewDay} minutesSinceLastRecordedPosition=${(minutesSinceLastRecordedPosition || 0).toFixed(2)} vesselStopped=${vesselStopped} `);
+
+        if (sog >= minimumSpeed && distance >= minimumDistance) {
+            addPositionToBuffer(position);
+            lastRecordedPosition = position;
+        }
+
+        // daily gpx
+        if (!recordVoyage
+            && bufferCount > 1
+            && isNewDay
+        ) {
+            app.debug('starting a new day - writing gpx file');
             try {
-                writeDailyGpxFile();
-                clearBuffer(true);
+                writeDailyGpxFile(getYyyymmdd(new Date(Date.now() - 24 * 60 * 60 * 1000))); // use yesterdays date
+                clearBuffer(true); // keep last row - so that the series of gpx files are gapless
             } catch (err) {
                 app.error(`Error writing GPX file: ${err}`);
             }
         }
 
-        var sog = app.getSelfPath("navigation.speedOverGround").value * KNOTS_PER_M_PER_S;
-
-        if (bufferCount == 0
-            || !minimumSpeed
-            || sog >= minimumSpeed) {
+        // gpx per voyage (not daily)
+        if (recordVoyage
+            && bufferCount > 1
+            && vesselStopped
+        ) {
+            app.debug('end of voyage detected - writing gpx file');
+            // record voyage end position
             addPositionToBuffer(position);
+            lastRecordedPosition = position;
+            try {
+                writeDailyGpxFile(getYyyymmddhhmm(new Date(lastRecordedPosition.ts)));
+                clearBuffer();
+            } catch (err) {
+                app.error(`Error writing GPX file: ${err}`);
+            }
         }
 
-        previousPosition = position;
     };
+
+    function validPosition(position) {
+        if (!position || !position.latitude || !position.longitude
+            || Math.abs(position.latitude) > 90
+            || Math.abs(position.longitude) > 180
+            // aparently some GPSs return small non-zero lat/lng when they lose fix, so:
+            || (Math.abs(position.latitude) <= 0.01 && Math.abs(position.longitude) <= 0.01)
+        ) {
+            return false;
+        }
+        return true;
+    }
 
     function addPositionToBuffer(position) {
         app.debug('Storing position in local buffer', position.ts, position.latitude, position.longitude);
@@ -257,7 +327,21 @@ module.exports = function (app) {
         updatePluginStatus();
     };
 
-    function writeDailyGpxFile() {
+    // e.g. 2025-03-17
+    function getYyyymmdd(date) {
+        return date.getFullYear() + '-'
+            + (date.getMonth() + 1).toString().padStart(2, "0") + '-'
+            + date.getDate().toString().padStart(2, "0");
+    }
+
+    // e.g. 2025-03-17-1426
+    function getYyyymmddhhmm(date) {
+        return getYyyymmdd(date) + '-'
+            + date.getHours().toString().padStart(2, "0")
+            + date.getMinutes().toString().padStart(2, "0");
+    }
+
+    function writeDailyGpxFile(name) {
         var trackPoints = db.prepare('SELECT * FROM buffer order by ts').all();
 
         if (!trackPoints || trackPoints.length == 0) {
@@ -276,14 +360,6 @@ module.exports = function (app) {
         var lastTrackPointDate = new Date(trackPoints[trackPoints.length - 1].ts)
         app.debug('lastTrackPointDate', lastTrackPointDate);
 
-        // e.g. 2025-03-17-1426
-        var trackName = lastTrackPointDate.getFullYear() + '-'
-            + (lastTrackPointDate.getMonth() + 1).toString().padStart(2, "0") + '-'
-            + lastTrackPointDate.getDate().toString().padStart(2, "0") + '-'
-            + lastTrackPointDate.getHours().toString().padStart(2, "0")
-            + lastTrackPointDate.getMinutes().toString().padStart(2, "0");
-
-        app.debug('trackName', trackName);
         var gpx = `<?xml version="1.0" encoding="UTF-8"?>
 <gpx xmlns="http://www.topografix.com/GPX/1/1" 
 version="1.1" creator="${plugin.name}"
@@ -291,7 +367,7 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1"
 xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd
 http://www.garmin.com/xmlschemas/TrackPointExtension/v1 https://www8.garmin.com/xmlschemas/TrackPointExtensionv1.xsd">
-<trk><name>${trackName}</name><trkseg>\n`;
+<trk><name>${name}</name><trkseg>\n`;
 
         for (let trackPoint of trackPoints) {
             gpx += `<trkpt lat="${trackPoint.latitude.toFixed(GPX_LAT_LONG_DECIMAL_PLACES)}" lon="${trackPoint.longitude.toFixed(GPX_LAT_LONG_DECIMAL_PLACES)}"><time>${new Date(trackPoint.ts).toISOString()}</time>`;
@@ -313,7 +389,7 @@ http://www.garmin.com/xmlschemas/TrackPointExtension/v1 https://www8.garmin.com/
             }
         }
 
-        filename = trackName + '.gpx';
+        filename = name + '.gpx';
         var fqFilename = path.join(gpxFolder, filename);
         app.debug('Writing gpx file', fqFilename);
         try {
@@ -330,8 +406,26 @@ http://www.garmin.com/xmlschemas/TrackPointExtension/v1 https://www8.garmin.com/
     function clearBuffer(keepLast) {
         app.debug('clearing buffer');
         db.prepare('DELETE FROM buffer' + (keepLast ? ' where ts not in (select ts from buffer order by ts desc limit 1)' : '')).run();
-        bufferCount = keepLast ? 1 : 0;
+        bufferCount = getBufferCount();
         updatePluginStatus();
+    };
+
+    function getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2) {
+        var R = 6371000; // Radius of the earth in m
+        var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+        var dLon = deg2rad(lon2 - lon1);
+        var a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2)
+            ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c; // Distance in m
+        return d;
+    };
+
+    function deg2rad(deg) {
+        return deg * (Math.PI / 180)
     };
 
     return plugin;
